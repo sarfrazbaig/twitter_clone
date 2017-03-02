e59eb8de29dc2d6aa0627793f709b98c73ae5a08
sarfrazbaig <sarfrazbaig55@gmail.com>
2017-03-03 02:35:17 +0530

    Landing page adjusted

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,25 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( theme.css )
+Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( pushy.css )
+Rails.application.config.assets.precompile += %w( masonry.css )
+Rails.application.config.assets.precompile += %w( animate.css )
+Rails.application.config.assets.precompile += %w( magnific-popup.css )
+Rails.application.config.assets.precompile += %w( odometer-theme-default.css )
+
+
+Rails.application.config.assets.precompile += %w( jquery.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( ie10-viewport-bug-workaround.js )
+Rails.application.config.assets.precompile += %w( masonry.js )
+Rails.application.config.assets.precompile += %w( pushy.min.js )
+Rails.application.config.assets.precompile += %w( jquery.magnific-popup.min.js )
+Rails.application.config.assets.precompile += %w( wow.min.js )
+Rails.application.config.assets.precompile += %w( scripts.js )
+Rails.application.config.assets.precompile += %w( odometer.js )
+
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
